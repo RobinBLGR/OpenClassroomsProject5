e4b9c8f384d44de5de8d1f674a5e6bd8de3e3a74
Robin Bellenguer <robin.bellenguer@gmail.com>
2024-03-15 11:11:03 +0100

    correctif 1

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -154,7 +154,7 @@
         }
       });
       next =
-        imagesCollection[index -1] ||
+        imagesCollection[index] ||
         imagesCollection[imagesCollection.length - 1];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
@@ -192,7 +192,7 @@
           index = i;
         }
       });
-      next = imagesCollection[index +1] || imagesCollection[0];
+      next = imagesCollection[index] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
@@ -240,7 +240,7 @@
         return;
       }
       $(".active-tag").removeClass("active active-tag");
-      $(this).addClass("active active-tag");
+      $(this).addClass("active-tag");
 
       var tag = $(this).data("images-toggle");
 
@@ -261,4 +261,3 @@
     }
   };
 })(jQuery);
-
